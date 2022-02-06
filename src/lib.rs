68ef4972e979c0580062136c199ddf9a5164b42d
Vinh Truong <vinhphuc.truong@gmail.com>
2022-02-07 01:16:37 +0200

    Use adt_const_params to clean up units

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,95 @@
 #![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
+#![feature(generic_const_exprs, adt_const_params)]
 
 use std::ops::*;
 
 // Dimension of any quantity Q is written in the form of a dimensional product:
-// > dim Q = LENGTH^a, MASS^b, TIME^c, ...
+//   dim Q = length^a, mass^b, time^c, ...
 // where the exponents a,b,c are signed integers.
-#[derive(Clone, Copy, PartialEq, PartialOrd, Debug)]
-pub struct Quantity<const LENGTH: i64, const MASS: i64, const TIME: i64>(f64);
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Unit {
+    length: i64,
+    mass: i64,
+    time: i64,
+}
 
-// Base units
-pub type Dimensionless = Quantity<0, 0, 0>;
-pub type Length = Quantity<1, 0, 0>;
-pub type Mass = Quantity<0, 1, 0>;
-pub type Time = Quantity<0, 0, 1>;
+impl Unit {
+    pub const fn add(self, rhs: Self) -> Self {
+        Self {
+            length: self.length + rhs.length,
+            mass: self.mass + rhs.mass,
+            time: self.time + rhs.time,
+        }
+    }
 
-// Derived units
-pub type Area = Quantity<2, 0, 0>;
-pub type Volume = Quantity<3, 0, 0>;
-pub type Velocity = Quantity<1, 0, -1>;
-pub type Acceleration = Quantity<1, 0, -2>;
-pub type Force = Quantity<1, 1, -2>;
-pub type Frequency = Quantity<0, 0, -1>;
-pub type Pressure = Quantity<-1, 1, -2>;
-pub type Energy = Quantity<2, 1, -2>;
-pub type Power = Quantity<2, 1, -3>;
+    pub const fn sub(self, rhs: Self) -> Self {
+        Self {
+            length: self.length - rhs.length,
+            mass: self.mass - rhs.mass,
+            time: self.time - rhs.time,
+        }
+    }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Quantity<LENGTH, MASS, TIME> {
-    pub fn format_units(self) -> String {
-        let value = self.0;
-        format!("{value:0.1} m^{LENGTH} kg^{MASS} s^{TIME}")
+    pub const fn neg(self) -> Self {
+        Self {
+            length: -self.length,
+            mass: -self.mass,
+            time: -self.time,
+        }
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> From<f64>
-    for Quantity<LENGTH, MASS, TIME>
-{
+#[derive(Clone, Copy, PartialEq, PartialOrd, Debug)]
+pub struct Quantity<const UNIT: Unit>(f64);
+
+macro_rules! quantity {
+    ($name: ident, $length:literal, $mass: literal, $time: literal) => {
+        pub type $name = Quantity<
+            {
+                Unit {
+                    length: $length,
+                    mass: $mass,
+                    time: $time,
+                }
+            },
+        >;
+    };
+}
+
+// Base units
+quantity!(Dimensionless, 0, 0, 0);
+quantity!(Length, 1, 0, 0);
+quantity!(Mass, 0, 1, 0);
+quantity!(Time, 0, 0, 1);
+
+// Derived units
+quantity!(Area, 2, 0, 0);
+quantity!(Volume, 3, 0, 0);
+quantity!(Velocity, 1, 0, -1);
+quantity!(Acceleration, 1, 0, -2);
+quantity!(Force, 1, 1, -2);
+quantity!(Frequency, 0, 0, -1);
+quantity!(Pressure, -1, 1, -2);
+quantity!(Energy, 2, 1, -2);
+quantity!(Power, 2, 1, -3);
+
+impl<const UNIT: Unit> Quantity<UNIT> {
+    pub fn format_units(self) -> String {
+        let value = self.0;
+        let length = UNIT.length;
+        let mass = UNIT.mass;
+        let time = UNIT.time;
+        format!("{value:0.1} m^{length} kg^{mass} s^{time}")
+    }
+}
+
+impl<const UNIT: Unit> From<f64> for Quantity<UNIT> {
     fn from(value: f64) -> Self {
         Self(value)
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Add for Quantity<LENGTH, MASS, TIME> {
+impl<const UNIT: Unit> Add for Quantity<UNIT> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -49,7 +97,7 @@ impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Add for Quantity<LENGT
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Sub for Quantity<LENGTH, MASS, TIME> {
+impl<const UNIT: Unit> Sub for Quantity<UNIT> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -57,23 +105,19 @@ impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Sub for Quantity<LENGT
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> AddAssign
-    for Quantity<LENGTH, MASS, TIME>
-{
+impl<const UNIT: Unit> AddAssign for Quantity<UNIT> {
     fn add_assign(&mut self, rhs: Self) {
         self.0 += rhs.0;
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> SubAssign
-    for Quantity<LENGTH, MASS, TIME>
-{
+impl<const UNIT: Unit> SubAssign for Quantity<UNIT> {
     fn sub_assign(&mut self, rhs: Self) {
         self.0 -= rhs.0;
     }
 }
 
-impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Neg for Quantity<LENGTH, MASS, TIME> {
+impl<const UNIT: Unit> Neg for Quantity<UNIT> {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
@@ -81,41 +125,36 @@ impl<const LENGTH: i64, const MASS: i64, const TIME: i64> Neg for Quantity<LENGT
     }
 }
 
-impl<
-        const LHS_LENGTH: i64,
-        const LHS_MASS: i64,
-        const LHS_TIME: i64,
-        const RHS_LENGTH: i64,
-        const RHS_MASS: i64,
-        const RHS_TIME: i64,
-    > Mul<Quantity<RHS_LENGTH, RHS_MASS, RHS_TIME>> for Quantity<LHS_LENGTH, LHS_MASS, LHS_TIME>
+impl<const LHS_UNIT: Unit, const RHS_UNIT: Unit> Mul<Quantity<RHS_UNIT>> for Quantity<LHS_UNIT>
 where
-    Quantity<{ LHS_LENGTH + RHS_LENGTH }, { LHS_MASS + RHS_MASS }, { LHS_TIME + RHS_TIME }>: Sized,
+    Quantity<{ LHS_UNIT.add(RHS_UNIT) }>: Sized,
 {
-    type Output =
-        Quantity<{ LHS_LENGTH + RHS_LENGTH }, { LHS_MASS + RHS_MASS }, { LHS_TIME + RHS_TIME }>;
+    type Output = Quantity<{ LHS_UNIT.add(RHS_UNIT) }>;
 
-    fn mul(self, rhs: Quantity<RHS_LENGTH, RHS_MASS, RHS_TIME>) -> Self::Output {
+    fn mul(self, rhs: Quantity<RHS_UNIT>) -> Self::Output {
         Quantity(self.0 * rhs.0)
     }
 }
 
-impl<
-        const LHS_LENGTH: i64,
-        const LHS_MASS: i64,
-        const LHS_TIME: i64,
-        const RHS_LENGTH: i64,
-        const RHS_MASS: i64,
-        const RHS_TIME: i64,
-    > Div<Quantity<RHS_LENGTH, RHS_MASS, RHS_TIME>> for Quantity<LHS_LENGTH, LHS_MASS, LHS_TIME>
+impl<const LHS_UNIT: Unit, const RHS_UNIT: Unit> Div<Quantity<RHS_UNIT>> for Quantity<LHS_UNIT>
 where
-    Quantity<{ LHS_LENGTH - RHS_LENGTH }, { LHS_MASS - RHS_MASS }, { LHS_TIME - RHS_TIME }>: Sized,
+    Quantity<{ LHS_UNIT.sub(RHS_UNIT) }>: Sized,
 {
-    type Output =
-        Quantity<{ LHS_LENGTH - RHS_LENGTH }, { LHS_MASS - RHS_MASS }, { LHS_TIME - RHS_TIME }>;
+    type Output = Quantity<{ LHS_UNIT.sub(RHS_UNIT) }>;
 
-    fn div(self, rhs: Quantity<RHS_LENGTH, RHS_MASS, RHS_TIME>) -> Self::Output {
+    fn div(self, rhs: Quantity<RHS_UNIT>) -> Self::Output {
         Quantity(self.0 / rhs.0)
+    }
+}
+
+impl<const UNIT: Unit> Div<Quantity<UNIT>> for f64
+where
+    Quantity<{ UNIT.neg() }>: Sized,
+{
+    type Output = Quantity<{ UNIT.neg() }>;
+
+    fn div(self, rhs: Quantity<UNIT>) -> Self::Output {
+        Quantity(self / rhs.0)
     }
 }
 
@@ -202,19 +241,17 @@ mod tests {
 
     #[test]
     fn frequency() {
-        let dimensionless = Dimensionless::from(1.0);
         let time = Time::from(1.0);
-        let frequency = dimensionless / time;
+        let frequency = 1.0 / time;
         assert_eq!(frequency, Frequency::from(1.0));
     }
 
     #[test]
     fn pressure() {
-        let dimensionless = Dimensionless::from(1.0);
         let length = Length::from(1.0);
         let mass = Mass::from(1.0);
         let time = Time::from(1.0);
-        let pressure = (dimensionless / length) * mass / (time * time);
+        let pressure = (1.0 / length) * mass / (time * time);
         assert_eq!(pressure, Pressure::from(1.0));
     }
 
